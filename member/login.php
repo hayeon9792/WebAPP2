5ffdab9b94f6e38ab50f3becbbee265c92d14197
yeonzel <hayeon9792@gmail.com>
2018-02-23 01:50:42 +0900

    로그인화면

---
+++ member/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+      <title>두 번째 페이지</title>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <link href="/web_project2/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+    <div class="container">
+      <header class="jumbotron text-center">
+        <h1>두 번째 페이지</h1>
+      </header>
+
+      <hr />
+
+      <article>
+      </article>
+
+      <hr />
+    </div>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+      <script src="/web_project2/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+    </body>
+    </html>
