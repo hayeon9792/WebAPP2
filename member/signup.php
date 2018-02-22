245f4341423b1b64a79bb06b41f1a0a0047dbc73
yeonzel <hayeon9792@gmail.com>
2018-02-23 00:02:00 +0900

    회원가입 페이지

---
+++ member/signup.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+    <title>두 번째 페이지</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="style2.css">
+  </head>
+  <body>
+    signup.php - 회원가입 페이지<br />
+    <hr />
+    
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="/web_project/first_project_180210/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+  </body>
+  </html>
