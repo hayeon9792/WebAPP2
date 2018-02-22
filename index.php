441f7641c3197212be8d4594bb4e23c4b1448126
yeonzel <hayeon9792@gmail.com>
2018-02-23 00:00:36 +0900

    두 번째 페이지 생성

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+    <title>두 번째 페이지</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="style2.css">
+  </head>
+  <body>
+    블로그용
+    <hr />
+    <a href="./member/signup.php">회원가입</a>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="/web_project/first_project_180210/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+  </body>
+  </html>
