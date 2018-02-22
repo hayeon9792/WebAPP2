eb0e2fdf61b83c29e7bdf4a9ad2785180781ad01
yeonzel <hayeon9792@gmail.com>
2018-02-23 00:26:09 +0900

    no message

--- member/signup.php
+++ member/signup.php
@@ -5,15 +5,17 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" type="text/css" href="style2.css">
+    <link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
-    signup.php - 회원가입 페이지<br />
-    <hr />
-    
+
+    <div class="container">
+      <form class="form-signup" action="signup-check.php" method="post">
+
+      </form>
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="/web_project/first_project_180210/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+    <script src="bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
   </body>
   </html>
