3024096a0541c33d2312a0f11c3e2f5ff6fa2c25
yeonzel <hayeon9792@gmail.com>
2018-02-23 01:50:32 +0900

    회원가입-완료

--- member/signup_check.php
+++ member/signup_check.php
@@ -10,11 +10,6 @@ if (mysqli_connect_error()) {
 extract($_POST); //자동으로 $user_id, $user_pw, $user_pw2, $user_email 변수 생성
 
 $encrypted_pw = sha1($user_pw);
-
-$q = "INSERT INTO ap_member(`id`,`pw`,`email`) VALUES('$user_id','$encrypted_pw','$user_email')";
-$mysqli -> query($q);
-//mysqli_query($mysqli, $q);
-$mysqli -> close();
 /*
 string md5 ( string $str [, bool $raw_output = false ] )
 $str = 해쉬값을 구할 문자열
@@ -26,4 +21,12 @@ $str = 해쉬값을 구할 문자열
 $raw_output = true 로 설정하면, 결과값이 20자로 만들어줍니다.
               설정하지 않을 경우, 40자의 길이를 가집니다.
 */
+
+$q = "INSERT INTO ap_member(`id`,`pw`,`email`) VALUES('$user_id','$encrypted_pw','$user_email')";
+$mysqli -> query($q);
+//mysqli_query($mysqli, $q);
+$mysqli -> close();
+
+header('Location: signup_done.php');
+exit();
  ?>
